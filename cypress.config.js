54be8a916afeb9d1476cb9f5e0c43b3b422e3518
farid-shaik-CTI-0119 <farid.shaik@coriolis.co.in>
2023-06-01 18:20:23 +0530

    added a test file todo.spec.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.baseUrl = "https://aqueous-brook-60480.herokuapp.com/";
+      config.specPattern = "cypress/integration/api-tests/*.spec.{js,jsx,ts,tsx}"
+      return config
     },
   },
 });
